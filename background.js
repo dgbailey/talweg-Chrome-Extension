c1d6c59a2271fb1e1c5b3cd4950167d82996ee3f
dgbailey <bailey.dust@gmail.com>
2019-04-24 00:13:07 -0700

    learning more additions to the manifest.

--- background.js
+++ background.js
@@ -9,3 +9,14 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("The color is green.");
   });
 });
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: 'developer.chrome.com'},
+    })
+    ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
+
