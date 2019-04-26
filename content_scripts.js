44bb218f9c228a9b0471a5e16bba67d1d5cb837d
dgbailey <bailey.dust@gmail.com>
2019-04-26 16:40:29 -0700

    wow learned alot about auto filling inputs with JS. Dispatching multiple events from the active element seemed to be the best way to work around the inconsistencies in login inputs across sites.  This is either bad design or purposeful security.

--- content_scripts.js
+++ content_scripts.js
@@ -1,15 +1,43 @@
 function checkClickedInputType() {
-    var x = document.activeElement.type;
-    var y = document.activeElement.tagName;
-    var e = new KeyboardEvent('keydown',{'keyCode':32,'which':32});
+    const ty = document.activeElement.type;
+    const tg = document.activeElement.tagName;
+    const elId = document.activeElement.id;
+    const e = new Event('keydown',{"bubbles":true, "cancelable":false});
+    const f = new Event("change", {"bubbles":true, "cancelable":false});
+    const g = new Event("focus", {"bubbles":true, "cancelable":false});
+    const h = new Event("blur", {"bubbles":true, "cancelable":false});
+    
+    
+    
+
+
+
    
-    console.log(x,y);
-    if( x === 'text'){
-    document.activeElement.focus();
-    document.activeElement.value = 'dustin login'}
-    if(y === 'INPUT' && x === 'password'){
-        document.activeElement.focus();
-        document.activeElement.value = 'r';}
+    console.log(ty,tg);
+    if( (tg === 'INPUT' && ty === 'text') || ((tg === 'INPUT' && ty === 'email'))){
+    
+        document.activeElement.click();
+        document.activeElement.value = 'bailey.dust@gmail.com';
+        document.activeElement.dispatchEvent(e);
+        document.activeElement.dispatchEvent(f);
+        document.activeElement.dispatchEvent(g);
+        document.activeElement.dispatchEvent(h);
+    }
+        //     console.log(elId);
+        //   //document get active element
+        
+        //     const againLogin = document.getElementById(elId);
+            
+        //     againLogin.value = 'dustin login'}
+
+    else if(tg === 'INPUT' && ty === 'password'){
+        
+        document.activeElement.value = 'r';
+        document.activeElement.dispatchEvent(e);
+        document.activeElement.dispatchEvent(f);
+        document.activeElement.dispatchEvent(g);
+        document.activeElement.dispatchEvent(h);
+    }
     
 
 }
